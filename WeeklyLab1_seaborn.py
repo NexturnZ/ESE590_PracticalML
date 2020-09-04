b1017c0d95aded00ef5e080d150d43eecbc185b1
ZLK <11510196@mail.sustech.edu.cn>
2020-09-04 12:21:31 +0800

    1.Problem with scatterplots fixed. The reason is that transformation from string type into float and int type is omitted. 2. Other plots finished. The lab is done.

--- WeeklyLab1_seaborn.py
+++ WeeklyLab1_seaborn.py
@@ -2,6 +2,7 @@
 import matplotlib.pyplot as plt
 import seaborn as sns
 import pandas as pd
+import numpy as np
 
 data_dir = 'data/abalone/abalone.data'
 labels = ['Male','Female','Infant'] # male, Femail, Infant
@@ -10,25 +11,49 @@ labels = ['Male','Female','Infant'] # male, Femail, Infant
 with open(data_dir) as f:
     lines = f.readlines()
 
+# extract features
 samples = []
-
 for line in lines:
-    # features = line.strip().split(',')
-    # samples.append(features[0:3])
-    samples.append(line.strip().split(','))
-
-# data = pd.DataFrame(samples,columns=['Sex','Length','Diameter'])
-
+    features = line.strip().split(',')
+    for i in range(7):
+        features[i+1] = float(features[i+1])
+    features[8] = int(features[8])
+    samples.append(features)
 
 data = pd.DataFrame(samples,columns=['Sex','Length','Diameter','Height', \
     'Whole_weight','Shucked_weight','Viscera_weight','Shell_weight','Rings'])
 
-#%% Scatter plot
+#%% Scatter plots for three dimension: length, diameter, height
+sns.pairplot(data,hue='Sex',vars=['Length','Diameter','Height'])
 
-# print(max(data['Diameter']))
+#%% Hexbin plots for length vs. rings
+hexbin = plt.figure()
+plt.hexbin(data['Length'], data['Rings'], gridsize=(10,10), cmap='Purples')
+plt.xlabel('Length')
+plt.ylabel('Rings')
 
-sns.pairplot(data,hue='Sex')
-# sns.distplot(data['Length'])
+#%% Density Plots for Whole_weight
+dist = plt.figure()
+sns.distplot(data['Whole_weight'])
+
+#%% Strip Plots for shucked weight
+strip = plt.figure()
+sns.stripplot(x='Sex', y='Shucked_weight', data=data, jitter=True)
+plt.grid()
+
+#%% Swarm Plots for viscera weight
+swarm = plt.figure()
+sns.swarmplot(x='Sex',y='Viscera_weight', data=data)
+plt.grid()
+
+#%% Box Plots for shell weight
+box = plt.figure()
+sns.boxplot(x='Sex',y='Shell_weight',data=data)
+plt.grid()
+
+#%% Bar Plots length vs. Rings
+bar = plt.figure()
+sns.barplot(x='Rings',y='Length',data=data)
 plt.show()
 
-
+# %%
