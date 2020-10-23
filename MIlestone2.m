951b0c95d80e2ce122c3fe74583939cb656324ef
NexturnZ <zhoulkmail@gmail.com>
2020-10-23 17:35:39 +0800

    both positive and negative peaks can be removed

--- MIlestone2.m
+++ MIlestone2.m
@@ -1,16 +1,34 @@
-df1 = load('./data/FHR/FHRDataCol.csv');
-df2 = load('./data/FHR/UADataCol.csv');
+clear;
+clc;
+%% Data loading
+HR = load('./data/FHR/FHRDataCol.csv'); % heart rate, size: 8000*552(PointsPerSample*Samples)
+UC = load('./data/FHR/UADataCol.csv'); % Uterus compression, size: 8000*552(PointsPerSample*Samples)
+
+num_samples = size(HR,2);
+train_HR = HR(:,1:num_samples/3);
+validation_HR = HR(:,num_samples/3+1:2*num_samples/3);
+test_HR = HR(:,2*num_samples/3+1:end);
+
+train_UC = UC(:,1:num_samples/3);
+validation_UC = UC(:,num_samples/3+1:2*num_samples/3);
+test_UC = UC(:,2*num_samples/3+1:end);
+
 
 %%
-ia = 7;
+ia = 120;
 Tstart = 0;
 Tstop = 2400;
-data = df1(1:Tstop,ia);
+t = linspace(0,600,2400);
+data = train_HR(1:Tstop,ia);
 
-p = 0.9;
-lambda = 1e4;
-z = baseline(data,lambda,p);
+p1 = 0.999;
+p2 = 0.001;
+lambda1 = 1e5;
+lambda2 = 1e6;
+neg_removed_baseline = baseline(data,lambda1,p1);
+pos_removed_baseline = baseline(neg_removed_baseline,lambda2,p2);
 figure(1);hold on;
-plot(data);
-plot(z);
+plot(t,data);
+plot(t,neg_removed_baseline);
+plot(t,pos_removed_baseline);
 
